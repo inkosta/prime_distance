981a032f6e6f67276e76b9f8874057d96162b0bd
inkosta <kinch@mail.ru>
2023-03-19 15:22:28 +0300

    modified:   prime_distance.py 	first experimental working version of primes calculating based on number of processes 	has a big overhead

--- prime_distance.py
+++ prime_distance.py
@@ -1,22 +1,34 @@
 import sys, time
+from sys import stdout as so
 import multiprocessing as mp
-import concurrent.futures as cf
-
+from threading import BrokenBarrierError
+from typing import Any
 
 NUMBER_OF_PROCESSES = mp.cpu_count()
 
 
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+def split_on_chunks(lst: list[Any], chunks_cnt: int) -> list[list[Any]]:
+    """Splits lst into n successive chunks."""
+    div, mod = divmod(len(lst),  chunks_cnt)
+    step = div + int(bool(mod))
+    return [lst[i:i + step] for i in range(0, len(lst), step)]
 
 
 # проверяет, является какое-нибудь число из списка dividers делителем num
-def func(num: int, dividers: list[int]) -> bool:
+def worker(num: int, dividers: list[int], barrier: mp.Barrier) -> bool:
+    # print(f"n: {num}, dividers: {dividers}")
     for div in dividers:
         if num % div == 0:
+            # print(f"\t{div} is divider of {num}")
+            barrier.abort()
             return True
+    # print(f"{dividers} are not dividers of {num}")
+    try:
+        barrier.wait()
+    except BrokenBarrierError:
+        # print("process aborted")
+        pass
+
     return False
 
 
@@ -24,34 +36,47 @@ def main(delta):
     primes = [3, 5, 7]
     d = 2
     n = 7
+    limit = 0
+    while primes[limit] ** 2 <= n:
+        limit += 1
+    while n < 500:
+        # while d < delta:
+        n += 2
+        # print(f"processed: {n}, ")
+        # so.flush()
+        while (limit < len(primes)) and (primes[limit]**2 < n):
+            limit += 1
+        chunks = split_on_chunks(primes[:limit + 1], NUMBER_OF_PROCESSES)
+        barrier = mp.Barrier(len(chunks) + 1)
+        workers = []
+        for chunk in chunks:
+            # print(f"worker({n}, {chunk}, barrier) ", end='...')
+            # so.flush()
+            w = mp.Process(target=worker, args=(n, chunk, barrier))
+            # print("created", end='...')
+            # so.flush()
+            w.start()
+            # print("started", end='...')
+            # so.flush()
+            workers.append(w)
+            # print("append to workers")
+            # so.flush()
 
-    with cf.ProcessPoolExecutor(max_workers=NUMBER_OF_PROCESSES) as executor:
-        while n < 1000:
-            # while d < delta:
-            n += 2
-            print(f"processed: {n}, ", end='')
-
-            futures = []
-            sr = int(n**0.5)
-            limit = 0
-            while limit < len(primes) and (primes[limit] < sr):
-                limit += 1
-            for chunk in chunks(primes[:limit], NUMBER_OF_PROCESSES):
-                w = executor.submit(func, n, chunk)
-                futures.append(w)
-            for future in cf.as_completed(futures):
-                if future.result():
-                    # n is not prime
-                    print("is not prime")
-                    break
-            else:
-                # n is prime
-                primes.append(n)
-                print("is prime")
+        try:
+            barrier.wait()
+        except BrokenBarrierError:
+            # print(f"number {n} is not prime")
+            [w.terminate() for w in workers]
+        else:
+            # print(f"number {n} is prime")
+            primes.append(n)
+            [w.join() for w in workers]
 
 
 if __name__ == '__main__':
-    mp.freeze_support()
+    # p = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53]
+    # print(split_on_chunks(p, 3))
+    # exit(0)
     delta = 2
     if len(sys.argv) > 1:
         print(sys.argv)
@@ -60,7 +85,8 @@ if __name__ == '__main__':
             print(f"delta = {delta}")
         except ValueError:
             print("Неверный аргумент! Используется значение по умолчанию: 2.")
-    start = time.process_time_ns()
+    start = time.time_ns()
+    print(f"start time: {start}")
     main(delta)
-    elapsed = (time.process_time_ns() - start) / 1_000_000_000
+    elapsed = (time.time_ns() - start) / 1_000_000_000
     print(f"elapsed: {elapsed}")
